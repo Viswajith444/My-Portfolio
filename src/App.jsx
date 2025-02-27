5b829c432f255de4b0c70e1677e7fe134b497dad
Viswajith444 <viswajith108@gmail.com>
2025-02-27 21:56:15 +0530

    made basic changes to Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,72 @@
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Navbar from "./components/navbar.jsx";
+import About from "./components/About.jsx";
+import "./App.css";
 
 function App() {
-  let items = [
-    { ref: "#", text: "Home" },
-    { ref: "#", text: "Services" },
-    { ref: "#", text: "Skills" },
-    { ref: "#", text: "Education" },
-    { ref: "#", text: "Experience" },
-    { ref: "#", text: "Contact" },
-  ];
+    let items = [
+        { ref: "#", text: "Home" },
+        { ref: "#", text: "Services" },
+        { ref: "#", text: "Skills" },
+        { ref: "#", text: "Education" },
+        { ref: "#", text: "Experience" },
+        { ref: "#", text: "Contact" },
+    ];
 
-  let dropdown = [
-    { ref: "#", text: "Will do" },
-    { text: "" },
-    { ref: "#", text: "Will do" },
-  ];
+    let dropdown = [
+        { ref: "#", text: "Will do" },
+        { text: "" },
+        { ref: "#", text: "Will do" },
+    ];
 
-  return (
-    <>
-      <Navbar items={items} dropdown={dropdown} thickness={3} />
+    return (
+        <>
+            <Navbar items={items} dropdown={dropdown} thickness={3} />
 
-      <main style={{ marginTop: "3rem", paddingTop: "1rem", height: "100vh" }}>
-        <p className="text-7xl text-amber-400">Hello World</p>
-        <span className="flex w-full items-center justify-center">
-          <span className="relative inline-block">
-            <span className="absolute -inset-x-10 -inset-y-3 block -skew-y-10 bg-linear-180 from-amber-300 to-amber-700"></span>
-            <span className="relative text-gray-700 hover:text-purple-500">
-              testing
-            </span>
-          </span>
-        </span>
-      </main>
-    </>
-  );
+            <main
+                style={{
+                    marginTop: "3rem",
+                    paddingTop: "1rem",
+                    height: "100vh",
+                }}
+            >
+               <About />
+
+                <span className="flex w-full items-center justify-center mt-100">
+                    <span className="relative inline-block">
+                        <span className="absolute -inset-x-10 -inset-y-3 block -skew-y-10 bg-linear-180 from-amber-300 to-amber-700"></span>
+                        <span className="relative text-2xl font-bold text-gray-700 hover:text-purple-500">
+                            testing
+                        </span>
+                    </span>
+                </span>
+
+                <div className="">
+                    <h2 className="text-xl text-black ">Heading</h2>
+                    <p className="text-black  not-hover:bg-amber-400">
+                        Hover over this paragraph to change the color of the
+                        next one.
+                    </p>
+                    <p className="text-black ">
+                        This paragraph will turn red when the previous one is
+                        hovered.
+                    </p>
+                    <p className="text-black ">
+                        This paragraph will turn red when the previous one is
+                        hovered.
+                    </p>
+                    <p className="text-black ">
+                        This paragraph will turn red when the previous one is
+                        hovered.
+                    </p>
+                    <p className="text-black ">
+                        This paragraph will turn red when the previous one is
+                        hovered.
+                    </p>
+                </div>
+            </main>
+        </>
+    );
 }
 
 export default App;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,51 @@
-import { useState } from "react";
+// import { useState } from "react";
+import profile from "../assets/user-solid.svg";
 
 function About() {
-  var [num, setnum] = useState(0);
-  return (
-    <div style={{ margin: "0 0 0 15px" }}>
-      <h1>About</h1>
-      <h2>state inital value is {0}</h2>
-      <button
-        onClick={() => setnum(num -1 )}
-      >
-        -
-      </button>
-      <h3>updating state: {num}</h3>
-      <button onClick={() => {
-          if (num === 5) {
-            setnum(100);
-          } else {
-            setnum(num + 1);
-          }
-        }}>+</button>
-      <br />
-      <button onClick={() => setnum(0)}>reset</button>
-    </div>
-  );
+    return (
+        <section className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+                <img
+                    src={profile}
+                    alt="Passport size photo"
+                    className="aspect-square h-full w-50 rounded-full border-2 border-gray-800 object-cover object-top"
+                />
+                <h1 className="mt-4 text-4xl font-bold text-black">
+                    {" "}
+                    Viswajith S
+                </h1>
+            </div>
+            <br />
+
+            <h1 className="text-4xl font-bold text-black mb-3">About</h1>
+            <p className="block w-[100ch] mx-auto text-center text-gray-400">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                labore, excepturi vitae veritatis hic commodi cum alias quos
+                officia temporibus, doloribus sapiente esse omnis nam eius
+                provident maiores. Doloribus ratione minima quo itaque, vel
+                omnis aut, rerum ipsam repellendus ipsa blanditiis minus natus
+                perferendis laborum hic sequi sunt! Totam sequi iure adipisci
+                exercitationem voluptatum, eum voluptates iste ullam ducimus
+                dignissimos? Incidunt rerum ullam, nesciunt eos voluptatum nam
+                commodi, id nulla obcaecati minima quia neque quibusdam
+                similique pariatur magnam explicabo rem eum reiciendis quaerat
+                quisquam. Et, dignissimos at minima totam ratione perspiciatis
+                repellat soluta iure iusto inventore nostrum tempore adipisci,
+                voluptate sit veniam iste sunt voluptatibus quos cupiditate
+                voluptas natus consequatur rem deserunt magni? Et dolor a soluta
+                eligendi quam distinctio eveniet corrupti atque. Quia corporis
+                sequi nostrum veniam non, magnam fugiat voluptatem dolores velit
+                ducimus dolore aut voluptas quam quis mollitia, obcaecati
+                perferendis, similique totam repudiandae officia nam quos? Quasi
+                consequuntur voluptatem tempora maiores eos recusandae
+                voluptatum nulla expedita! Doloremque nihil sequi ab quae quo,
+                harum ducimus mollitia facere id culpa consectetur, voluptatibus
+                minima tempora, quaerat esse animi reprehenderit eos reiciendis
+                pariatur dolorum adipisci nesciunt? Illum voluptatum et placeat
+                quaerat consectetur aperiam. Nam reiciendis, sapiente amet
+                aliquam ipsam architecto minima.
+            </p>
+        </section>
+    );
 }
 export default About;
